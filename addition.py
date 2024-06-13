053c5eb0526de34b787a32868d4991b16f24860e
Garbitt <jules.garbay@outlook.fr>
2024-06-13 15:06:32 +0200

    AddBest: Commentaire ajouté

--- addition.py
+++ addition.py
@@ -9,5 +9,11 @@ def add(x: int, y: int) -> int:
     return x+y
 
 
-def add_float(x: float, y: float)->float:
+def add_float(x: float, y: float) -> float:
+    '''
+    Commentaire ajouté sur l'addition
+    :param x:
+    :param y:
+    :return:
+    '''
     return x+y
