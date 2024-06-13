3ee8d9dcfa65efa43a31838a54ae0be63cd6ac2d
Garbitt <jules.garbay@outlook.fr>
2024-06-13 15:15:45 +0200

    MultBest: Correction de commantaire

--- multiplication.py
+++ multiplication.py
@@ -1,19 +1,19 @@
 
 def mult(x: int, y: int) -> int:
-    '''
+    """
     Commentaire pour la multiplication important
     :param x:
     :param y:
     :return:
-    '''
+    """
     return x*y
 
 
-def mult_float(x: float, y: float)->float:
-    '''
+def mult_float(x: float, y: float) -> float:
+    """
     Extra com
     :param x:
     :param y:
     :return:
-    '''
+    """
     return x*y
