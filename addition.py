8eaf9b09638e382b21b1ac2cb3edb7e01f658f88
rodriguerillardon <rodrigue.rillardon@laposte.net>
2024-06-26 15:56:38 +0200

    modification du commentaire addition pour la doc

--- addition.py
+++ addition.py
@@ -1,13 +1,20 @@
 
 def add(x: int, y: int) -> int:
+    '''
+    Cette fonction sert à faire une addition
+    :param x: le paramètre x est un entier
+    :param y: le paramètre y est un entier
+    :return: l'addtion de x et y
+    '''
     return x+y
 
 
 def add_float(x: float, y: float) -> float:
     '''
     Commentaire ajouté sur l'addition
-    :param x:
-    :param y:
-    :return:
+    Cette fonction sert à faire une addition
+    :param x: le paramètre x est un float
+    :param y: le paramètre y est un float
+    :return: l'addtion de x et y
     '''
     return x+y
